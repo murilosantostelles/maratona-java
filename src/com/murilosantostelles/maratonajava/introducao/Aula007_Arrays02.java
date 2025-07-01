982aaedd230e0b5c4af74a27819f5ef00eba8496
murilosantostelles <murilosantos.work@gmail.com>
2025-06-30 23:00:55 -0300

    Aula 007 - Arrays 03

--- src/com/murilosantostelles/maratonajava/introducao/Aula007_Arrays02.java
+++ src/com/murilosantostelles/maratonajava/introducao/Aula007_Arrays02.java
@@ -7,7 +7,12 @@ public class Aula007_Arrays02 {
         // boolean false
         // String null
 
-        String[] names = new String[3];
-        System.out.println(names[0]);
+        String[] names = new String[4];
+        names[0] = "Murilo";
+        names[1] = "Santos";
+        names[2] = "Telles";
+        for(int i = 0; i < names.length; i++){ // lenght é como a função len do python.
+            System.out.println(names[i]);
+        }
     }
 }
