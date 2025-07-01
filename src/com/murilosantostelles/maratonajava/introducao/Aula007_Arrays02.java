cfc2f148d6e5e5703df4902059ac3bc0437859f9
murilosantostelles <murilosantos.work@gmail.com>
2025-06-30 22:48:51 -0300

    feat: Aula 007 - Arrays 02

---
+++ src/com/murilosantostelles/maratonajava/introducao/Aula007_Arrays02.java
@@ -0,0 +1,13 @@
+package com.murilosantostelles.maratonajava.introducao;
+
+public class Aula007_Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double 0
+        // char '/u0000' ' '
+        // boolean false
+        // String null
+
+        String[] names = new String[3];
+        System.out.println(names[0]);
+    }
+}
