0761ad9869d0f75c6aff559611ffeeee682ce017
murilosantostelles <murilosantos.work@gmail.com>
2025-06-30 22:39:16 -0300

    feat: Aula 007 - Arrays 01

---
+++ src/com/murilosantostelles/maratonajava/introducao/Aula007_Arrays01.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.introducao;
+
+public class Aula007_Arrays01 {
+    public static void main(String[] args) {
+        int [] idades = new int[3]; // cria um array com três espaços
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 11;
+        System.out.println(idades[1]);
+    }
+}
