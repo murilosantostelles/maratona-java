b430ccfcc690655fe19dd91927b55c8d3bf65f63
murilosantostelles <murilosantos.work@gmail.com>
2025-07-01 21:41:19 -0300

    feat: Orientação a Objetos - Métodos pt 01

--- src/com/murilosantostelles/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Aintroducaoclasses/test/CarroTest01.java
@@ -7,6 +7,9 @@ public class CarroTest01 {
         Carro carro = new Carro();
         Carro carro2 = new Carro();
 
+        // para o carro 1 fazer referência ao carro 2, por exemplo,
+        // basta digitar carro = carro2, ou vice versa.
+
         carro.marca = "Volkswagen";
         carro.modelo = "T-Cross";
         carro.ano = 2020;
@@ -15,7 +18,7 @@ public class CarroTest01 {
         carro2.modelo = "320i";
         carro2.ano = 2022;
 
-        System.out.println("Marca: "+carro.marca +"| Modelo: "+carro.modelo+"| Ano: "+carro.ano);
-        System.out.println("Marca: "+carro2.marca +"| Modelo: "+carro2.modelo+"| Ano: "+carro2.ano);
+        System.out.println("Marca: "+carro.marca +" | Modelo: "+carro.modelo+"| Ano: "+carro.ano);
+        System.out.println("Marca: "+carro2.marca +" | Modelo: "+carro2.modelo+"| Ano: "+carro2.ano);
     }
 }
---
+++ src/com/murilosantostelles/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros(){ // void(vazio) indica que nao retorna nada.
+        System.out.println(10 + 10);
+    }
+    public void subtracaoDoisNumeros(){
+        System.out.println(20-10);
+    }
+}
