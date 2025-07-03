8070ae5aca78453bc313adbea8f18bf055915412
murilosantostelles <murilosantos.work@gmail.com>
2025-07-02 22:56:58 -0300

    feat: Orientaçãoa a Objetos - Modificador de acesso private, get e set pt o2

--- src/com/murilosantostelles/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
+++ src/com/murilosantostelles/maratonajava/javacore/Bintroducaometodos/dominio/Pessoa.java
@@ -5,12 +5,8 @@ public class Pessoa {
     private String nome; // colocar private significa que esses atributos só podem ser acessados pelo objeto. Fora daqui não dá pra acessar
     private int idade;
 
-    public void imprimir(){
-        System.out.println(this.nome);
-        System.out.println(this.idade);
-    }
 
-    //                               SET
+    //                               SET -> leva pro original em memória
     public void setNome(String nome){ // mETODO publico pra acessar os atributos privados.
         this.nome = nome;
     }
@@ -23,7 +19,7 @@ public class Pessoa {
         this.idade = idade;
     }
 
-    //                                         GET
+    //                                         GET -> pega oq foi setado no original em memoria
 
     public String getNome(){
         return this.nome;
