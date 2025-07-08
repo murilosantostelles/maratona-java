046adc71e9d071f8f0823604859a1b2f42ca570b
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-07-08 00:40:22 -0300

    feat: Orientação a Objetos - Construtores pt 02 - Sobrecarga

--- src/com/murilosantostelles/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/com/murilosantostelles/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,17 +5,18 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime(String nome , String tipo , int episodios, String genero){ //construtor
-        System.out.println("Dentro do construtor");
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public Anime(){
-
+    public Anime(String nome , String tipo , int episodios, String genero, String estudio){
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
     public void imprime(){
@@ -23,6 +24,7 @@ public class Anime {
         System.out.println(this.getTipo());
         System.out.println(this.getEpisodios());
         System.out.println(this.getGenero());
+        System.out.println(this.estudio);
     }
 
 
--- src/com/murilosantostelles/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -4,13 +4,12 @@ import com.murilosantostelles.maratonajava.javacore.Dconstrutores.dominio.Anime;
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime("Haikyuu", "TV" , 33, "Esporte");
-        Anime anime2 = new Anime();
+        Anime anime = new Anime("Haikyuu", "TV" , 33, "Esporte", "Production IG");
+
 
 
         //anime.init("Vinland Saga" , "TV" , 74);
         //anime.init("Vinland Saga" , "TV" , 74, "Seinen");
         anime.imprime();
-        anime2.imprime();
     }
 }
