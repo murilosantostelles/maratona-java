4e52d3587c0182ac67b279017549930d7dde0141
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-07-16 19:21:51 -0300

    feat: Orientação a Objetos - Blocos de Inicialização

--- src/com/murilosantostelles/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
+++ src/com/murilosantostelles/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -3,14 +3,45 @@ package com.murilosantostelles.maratonajava.javacore.Eblocosinicializacao.domini
 public class Anime {
     private String nome;
     private int[] episodios;
+    // Ordem:
+    // 1 - Alocado espaço em memoria pro objeto
+    // 2- Cada atributo da classe é criado e inicializado com valores default ou o quer for passada
+    // 3 - Bloco de Inicialização é executado
+    // 4- Construtor é executado.
+    // Diferença entre bloco de inicialização e construtor:
+    // o bloco é executado independente do construtor que vc chamar
 
-    public Anime() {
-        episodios = new int[100];
+
+    {
+        System.out.println("Dentro do Bloco de Inicialização");
+        episodios = new int [100];
         for (int i = 0; i < episodios.length; i++) {
             episodios[i] = i+1;
         }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime(int[] episodios) {
+        this.episodios = episodios;
+    }
+
+    public Anime() {
+        episodios = new int[100];
+
         for(int episodio:this.episodios){
             System.out.print(episodio+" ");
         }
+        System.out.println(" ");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
     }
 }
--- src/com/murilosantostelles/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Eblocosinicializacao/test/AnimeTest01.java
@@ -4,6 +4,11 @@ import com.murilosantostelles.maratonajava.javacore.Eblocosinicializacao.dominio
 
 public class AnimeTest01 {
     public static void main(String[] args) {
-        Anime anime = new Anime();
+        Anime anime = new Anime("One Piece");
+
+        for (int episodio : anime.getEpisodios()) {
+            System.out.println(episodio+" ");
+        }
+
     }
 }
