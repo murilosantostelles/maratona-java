9110c712e65a15c0b4cefaebeb53734a59381c09
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-19 10:23:55 -0300

    feat: Orientação a Objetos - Bloco de Inicialização estático

---
+++ src/com/murilosantostelles/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.javacore.Fmodificadorestatico.test;
+
+import com.murilosantostelles.maratonajava.javacore.Fmodificadorestatico.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+    }
+}
