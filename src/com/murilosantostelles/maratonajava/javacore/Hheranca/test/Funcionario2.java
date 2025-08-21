a5fcc6f82fe9120ab45db3071e11976535056014
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-21 11:22:36 -0300

    feat: OOP - Herança pt 03 - Protected

---
+++ src/com/murilosantostelles/maratonajava/javacore/Hheranca/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package com.murilosantostelles.maratonajava.javacore.Hheranca.test;
+
+import com.murilosantostelles.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class Funcionario2  extends Pessoa {
+    public void imprime(){
+        System.out.println(this.nome);
+    }
+}
