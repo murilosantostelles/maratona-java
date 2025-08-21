60da777064057a360924d5f9d58406d915db765c
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-21 10:54:51 -0300

    feat: OOP - Herança pt 02 - Super

--- src/com/murilosantostelles/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/com/murilosantostelles/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -3,6 +3,11 @@ package com.murilosantostelles.maratonajava.javacore.Hheranca.dominio;
 public class Funcionario extends Pessoa {
     private double salario;
 
+    public void imprime(){
+        super.imprime(); //sobescrita de met odo. usa-se "super" para usar o metodo da classe pai, adicionando, nesse caso a impressão do salario(que nao tem na classe pai).
+        System.out.println(this.salario);
+    }
+
     public double getSalario() {
         return salario;
     }
