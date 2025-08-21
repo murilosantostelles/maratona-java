e88d3610c47f7fa69f8eb57de769e9a3b8c9dc93
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-21 10:42:40 -0300

    feat: OOP - Herança pt 01

---
+++ src/com/murilosantostelles/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,28 @@
+package com.murilosantostelles.maratonajava.javacore.Hheranca.test;
+
+import com.murilosantostelles.maratonajava.javacore.Hheranca.dominio.Endereco;
+import com.murilosantostelles.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import com.murilosantostelles.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco1 = new Endereco();
+        endereco1.setRua("Rua 3");
+        endereco1.setCep("2232-123");
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setNome("Murilo");
+        pessoa1.setCpf("123456");
+        pessoa1.setEndereco(endereco1);
+
+        pessoa1.imprime();
+
+        System.out.println("----------------");
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setNome("Gepeto");
+        funcionario1.setCpf("12344566");
+        funcionario1.setEndereco(endereco1);
+        funcionario1.setSalario(20000);
+
+        funcionario1.imprime();
+    }
+}
