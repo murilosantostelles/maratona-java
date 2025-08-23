9f8282786260902ef951b32c593bf30293540b70
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-22 22:14:46 -0300

    feat: OOP - método toString

---
+++ src/com/murilosantostelles/maratonajava/javacore/Isobescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.javacore.Isobescrita.test;
+
+import com.murilosantostelles.maratonajava.javacore.Isobescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime("Dr. Stone");
+
+        System.out.println(anime1);
+    }
+}
