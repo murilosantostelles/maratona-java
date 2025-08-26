b062a3954acb8b5fd9ba85e4ad8dddeb6b870331
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-26 14:37:26 -0300

    feat: OOP - classes abstratas pt 01

---
+++ src/com/murilosantostelles/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,13 @@
+package com.murilosantostelles.maratonajava.javacore.Lclassesabstratas.dominio;
+
+public abstract class Funcionario {
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+
+}
---
+++ src/com/murilosantostelles/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,14 @@
+package com.murilosantostelles.maratonajava.javacore.Lclassesabstratas.test;
+
+import com.murilosantostelles.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import com.murilosantostelles.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import com.murilosantostelles.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente01 = new Gerente("Nami" , 5000);
+        Desenvolvedor desenvolvedor01 = new Desenvolvedor("Oto" , 12000);
+        System.out.println(gerente01);
+        System.out.println(desenvolvedor01);
+    }
+}
