526b90262f7f41f7ad51a1cab1e20101646f9526
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-08-29 11:14:47 -0300

    feat: OOP - Polimorfismo pt 03 - Parâmetros Polimórficos

--- src/com/murilosantostelles/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ src/com/murilosantostelles/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,24 +1,17 @@
 package com.murilosantostelles.maratonajava.javacore.Npolimorfismo.servico;
 
 import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcuarImpostoComputador(Computador computador){
-        System.out.println("Relatório de imposto do computador");
-        double imposto = computador.calcularImposto();
-        System.out.println("Nome do computador: "+computador.getNome());
-        System.out.println("Valor do computador: "+computador.getValor());
-        System.out.println("Imposto do computador: "+computador.calcularImposto());
 
-    }
-
-    public static void calcuarImpostoTomate(Tomate tomate){
-        System.out.println("Relatório de imposto do tomate");
-        double imposto = tomate.calcularImposto();
-        System.out.println("Nome do tomate: "+tomate.getNome());
-        System.out.println("Valor do tomate: "+tomate.getValor());
-        System.out.println("Imposto do tomate: "+tomate.calcularImposto());
+    public static void calcularImposto(Produto produto){
+        System.out.println("Relatório de imposto do produto:");
+        double imposto = produto.calcularImposto();
+        System.out.println("Nome do produto: "+produto.getNome());
+        System.out.println("Valor do produto: "+produto.getValor());
+        System.out.println("Imposto a ser pago: "+imposto);
 
     }
 }
--- src/com/murilosantostelles/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Npolimorfismo/test/ProdutoTest01.java
@@ -1,6 +1,7 @@
 package com.murilosantostelles.maratonajava.javacore.Npolimorfismo.test;
 
 import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import com.murilosantostelles.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTest01 {
     public static void main(String[] args) {
         Computador computador01 = new Computador("NUC10i7" , 11000);
         Tomate tomate01 = new Tomate("Tomate Siciliano" , 10);
-        CalculadoraImposto.calcuarImpostoComputador(computador01);
+        Televisao televisao01 = new Televisao("LG 2025" , 4000);
+        CalculadoraImposto.calcularImposto(computador01);
         System.out.println("------------------");
-        CalculadoraImposto.calcuarImpostoTomate(tomate01);
-
+        CalculadoraImposto.calcularImposto(tomate01);
+        System.out.println("------------------");
+        CalculadoraImposto.calcularImposto(televisao01);
     }
 }
+
