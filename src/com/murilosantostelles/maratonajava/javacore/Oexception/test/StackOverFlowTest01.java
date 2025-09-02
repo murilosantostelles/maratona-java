b2b9077f5382d55a7f3cdbed3ccb0d753441667f
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-02 13:12:19 -0300

    feat: Exceções pt 01 - Errors

---
+++ src/com/murilosantostelles/maratonajava/javacore/Oexception/test/StackOverFlowTest01.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.javacore.Oexception.test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade(){
+        recursividade(); //mé t odo se chamando varias vezes, até que em um momento da um "StackOverFowError"//
+    }
+}
