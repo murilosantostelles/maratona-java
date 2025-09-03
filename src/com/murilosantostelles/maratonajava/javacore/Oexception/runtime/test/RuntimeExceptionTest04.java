d107f9f5d3be81312acd98eb66df30b6fd439467
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-03 17:23:02 -0300

    feat: Exceptions pt 08 - Multi catch em linha

--- src/com/murilosantostelles/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ src/com/murilosantostelles/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -1,15 +1,29 @@
 package com.murilosantostelles.maratonajava.javacore.Oexception.runtime.test;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
         try{
             throw new RuntimeException();
-        }catch (ArrayIndexOutOfBoundsException e){
-            System.out.println("Denttro do ArrayIndexOutOfBoundsException");
+        }catch (ArrayIndexOutOfBoundsException  | IllegalArgumentException | ArithmeticException e){
+            System.out.println("Denttro do ArrayIndexOutOfBoundsException ... ...");
         }catch (IndexOutOfBoundsException e){
             System.out.println("dentro do IndexOutOfBoundsException");
         } catch (RuntimeException e) {
             System.out.println("dentro do RuntimeException");
         }
+        try {
+            talvezLanceException();
+        } catch (SQLException | IOException e){ // 2 excessõespodem ser escritas juntas
+            e.printStackTrace();
+        }
+
     }
+
+    private static void talvezLanceException() throws SQLException, IOException{
+
+        }
 }
