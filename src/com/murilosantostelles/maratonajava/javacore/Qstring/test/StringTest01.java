6428a3e91736b3395207586c8d0bd865aefb928b
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-04 17:03:36 -0300

    feat: Classes Utilitárias - Strings pt 02

---
+++ src/com/murilosantostelles/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,10 @@
+package com.murilosantostelles.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "William"; // String constant pool
+        String nome2 = "William"; // String constant pool
+
+
+    }
+}
