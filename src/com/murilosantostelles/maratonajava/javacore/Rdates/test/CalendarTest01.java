e480182747c7de4c95748f0144e2786dc4413556
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-08 00:58:58 -0300

    feat: Classes Utilitárias - DateFOrmat

--- src/com/murilosantostelles/maratonajava/javacore/Rdates/test/CalendarTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Rdates/test/CalendarTest01.java
@@ -7,15 +7,12 @@ public class CalendarTest01 {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
 
-        
-
-
 
         System.out.println(calendar); // forma não formatada.
 
         System.out.println("------------------");
 
-        if(calendar.getFirstDayOfWeek() == calendar.SUNDAY){
+        if (calendar.getFirstDayOfWeek() == calendar.SUNDAY) {
             System.out.println("Domingo é o primeiro dia da semana");
         }
 
@@ -23,7 +20,7 @@ public class CalendarTest01 {
         System.out.println(calendar.get(calendar.DAY_OF_YEAR));
 
         // alterando data, adicionando dois dias ao dia de hj
-        calendar.add(Calendar.DAY_OF_MONTH,2);
+        calendar.add(Calendar.DAY_OF_MONTH, 2);
         System.out.println("------------------");
         Date date = calendar.getTime();
         System.out.println(date);
