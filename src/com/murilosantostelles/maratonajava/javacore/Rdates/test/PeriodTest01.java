7e324d048e4dba5f1ccbe99fdb6d13debb8595ac
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-08 17:23:16 -0300

    feat: Classes Utilitárias - Period

---
+++ src/com/murilosantostelles/maratonajava/javacore/Rdates/test/PeriodTest01.java
@@ -0,0 +1,18 @@
+package com.murilosantostelles.maratonajava.javacore.Rdates.test;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class PeriodTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2).plusDays(7);
+        Period p1 = Period.between(now , nowAfterTwoYears); // só aceita localDate
+
+        Period p2 = Period.ofWeeks(325);
+
+
+        System.out.println(p1);
+        System.out.println(p2);
+    }
+}
