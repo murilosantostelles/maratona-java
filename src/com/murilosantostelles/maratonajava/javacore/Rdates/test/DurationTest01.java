115c1522ce0cdc7429643aedc4065731ecc8668f
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-08 17:16:12 -0300

    feat: Classes Utilitárias - Duration

---
+++ src/com/murilosantostelles/maratonajava/javacore/Rdates/test/DurationTest01.java
@@ -0,0 +1,22 @@
+package com.murilosantostelles.maratonajava.javacore.Rdates.test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        // Duração
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2);
+        LocalTime timenow = LocalTime.now();
+        LocalTime timeMinusSevenHours = LocalTime.now().minusHours(7);
+        Duration d1 = Duration.between(now, nowAfterTwoYears);
+        Duration d2 = Duration.between(timenow, timeMinusSevenHours);
+
+
+        System.out.println(Duration.ofDays(20)); // duração de 20 dias
+        System.out.println(d1);
+        System.out.println(d2);
+    }
+}
