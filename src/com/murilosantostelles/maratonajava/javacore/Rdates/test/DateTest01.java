35735283e644b0207d3fded0fb89357c8f78a469
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-07 22:45:23 -0300

    feat: Classes Utilitárias - Dates

---
+++ src/com/murilosantostelles/maratonajava/javacore/Rdates/test/DateTest01.java
@@ -0,0 +1,11 @@
+package com.murilosantostelles.maratonajava.javacore.Rdates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        // classe date trabalha com variavel tipo long
+        System.out.println(date);
+    }
+}
