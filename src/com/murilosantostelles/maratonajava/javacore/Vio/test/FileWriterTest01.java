ad5ab3409571be09c9331be4f079bed8551be352
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-10 13:13:00 -0300

    feat: Classes Utilitárias - IO pt 03 - FileReader

--- src/com/murilosantostelles/maratonajava/javacore/Vio/test/FileWriterTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Vio/test/FileWriterTest01.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 // FileReader
 // BufferedWriter
 // BufferedReader
-public class FileWritterTest01 {
+public class FileWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true)){
