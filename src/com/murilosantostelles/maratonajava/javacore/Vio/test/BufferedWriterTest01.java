477693062364f5866fd9904237e4f36279d2fc6e
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-10 13:39:48 -0300

    feat: Classes Utilitárias - IO pt 05 - BufferedReader

--- src/com/murilosantostelles/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -9,10 +9,10 @@ public class BufferedWriterTest01 { // PERFORMANCE MELHOR
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)){
-            br.write("O DevDojo é o melhor curso do Brasil ");
-            br.newLine();
-            br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)){
+            bw.write("O DevDojo é o melhor curso do Brasil ");
+            bw.newLine();
+            bw.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
