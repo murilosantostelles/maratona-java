9a23aabac9113708ac5004c7706144e70bb458ea
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-24 14:36:00 -0300

    feat: Coleções pt 04 - hashCode

--- src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/dominio/Smartphone.java
+++ src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/dominio/Smartphone.java
@@ -23,6 +23,15 @@ public class Smartphone {
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber) && marca.equals(smartphone.marca);
     }
 
+
+    // se x.equals(y) == true, y.hashCode() tem que ser == x.hashCode()
+    // y.hashCode == x.hashCode() não necessarioamente o equals de y.equals(x) tem que ser true. Se os hascodes forem iguals, não necessariamente o equals tem que ser true
+    // y.hashCode() != x.hashCode(), x.equals(y) deverá ser false.
+    @Override
+    public int hashCode() {
+        return this.serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
