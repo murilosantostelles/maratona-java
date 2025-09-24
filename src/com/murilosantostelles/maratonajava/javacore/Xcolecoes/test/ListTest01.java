4adb42a742c5393faf9547b898c6486ae6c6f881
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-24 18:19:14 -0300

    feat: Coleções pt 07 - List pt 02

--- src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/ListTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/ListTest01.java
@@ -6,17 +6,22 @@ import java.util.List;
 public class ListTest01 {
     public static void main(String[] args) {
         List<String> nomes = new ArrayList<>(); // um array que pode ser dinamicamente incrementado
+        List<String> nomes2 = new ArrayList<>(); // um array que pode ser dinamicamente incrementado
         nomes.add("William");
         nomes.add("DevDojo");
+        nomes2.add("Suane");
+        nomes2.add("Academy");
+
+        nomes.addAll(nomes2);
 
         for (Object nome : nomes) {
             System.out.println(nome);
         }
+
         System.out.println("----------");
 
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
         }
-
     }
 }
