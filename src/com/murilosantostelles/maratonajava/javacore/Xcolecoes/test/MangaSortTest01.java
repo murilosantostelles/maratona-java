b2349940dc732889dcf945a274379246f76363c0
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-26 00:37:36 -0300

    feat: Coleções pt 11 - Sorting Lists pt 03 - Comparator

--- src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/MangaSortTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/MangaSortTest01.java
@@ -3,7 +3,16 @@ package com.murilosantostelles.maratonajava.javacore.Xcolecoes.test;
 import com.murilosantostelles.maratonajava.javacore.Xcolecoes.dominio.Manga;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+class MangaByNameComparator implements Comparator<Manga>{
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getNome().compareTo(manga2.getNome());
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -14,6 +23,24 @@ public class MangaSortTest01 {
         mangas.add(new Manga(3, "Attack on titan", 11.20));
         mangas.add(new Manga(2, "Dragon ball Z", 2.99));
 
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        Collections.sort(mangas);
+        System.out.println("-----------");
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        Collections.sort(mangas, new MangaByNameComparator());
+        System.out.println("-----------");
+
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
 
     }
 }
