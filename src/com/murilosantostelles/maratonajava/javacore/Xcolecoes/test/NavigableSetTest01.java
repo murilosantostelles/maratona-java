31a7151a3bbfd8cd349b7a452bad987b9291a960
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-29 09:18:51 -0300

    feat: Coleções pt 17 - NavigableSet, TreeSet pt 02

--- src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/NavigableSetTest01.java
+++ src/com/murilosantostelles/maratonajava/javacore/Xcolecoes/test/NavigableSetTest01.java
@@ -15,13 +15,21 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone>{
         return smartphone.getMarca().compareTo(t1.getMarca());
     }
 }
+
+class MangaPrecoComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga manga, Manga t1) {
+        return Double.compare(manga.getPreco(),t1.getPreco());
+    }
+}
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L,"hellsing ultimate",19.9, 0)); // criando um objeto diretamente do método "add"
         mangas.add(new Manga(1L, "Berserk", 9.5, 5));
         mangas.add(new Manga(4L, "Pokemon", 3.2, 0));
@@ -33,5 +41,18 @@ public class NavigableSetTest01 {
             System.out.println(manga);
         }
         // o TreeSet vai automaticamente fazer a organização da sua coleção baseado no compareTo() da classe! No caso está ordennado po id 1 -> 5, pois o compare to se baseia no id
+
+
+        System.out.println("---------------");
+        for (Manga manga : mangas.descendingSet()) { // ordem descrescente
+            System.out.println(manga);
+        }
+
+
+        // metodos de comparação interessantes:
+        // lower: vai trazer o objeto que é imdiatamente menor do que o valor q vc passa
+        // floor: vai trazer o mesmo valor que vc passa <=
+        // higher: vai trazer o objeto que é imediatamente maior do que o valor q vc passa
+        // ceiling: vai trazer o mesmo valor que vc passa >=
     }
 }
