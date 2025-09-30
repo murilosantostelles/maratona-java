e64c8b1d03b50a66672ed21337c4f985b6e9b3f2
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-30 11:08:08 -0300

    feat: Generics pt 01 - Introdução

---
+++ src/com/murilosantostelles/maratonajava/javacore/YGenerics/test/GenericsTest01.java
@@ -0,0 +1,30 @@
+package com.murilosantostelles.maratonajava.javacore.YGenerics.test;
+
+import com.murilosantostelles.maratonajava.javacore.Xcolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest01 {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Midoriya");
+        lista.add("Midoriya");
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+
+        add(lista, new Consumidor(10L, "Midoriya"));
+
+        for (String o : lista) {
+            System.out.println(o);
+        }
+
+
+    }
+
+    private static void add(List lista, Consumidor consumidor){
+        lista.add(consumidor);
+    }
+}
