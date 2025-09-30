76aab17270a1c12385183d853c7548f5872f50c6
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-09-30 11:55:25 -0300

    feat: Generics pt 03 - Wildcard pt 02

--- src/com/murilosantostelles/maratonajava/javacore/YGenerics/test/WildCardTest02.java
+++ src/com/murilosantostelles/maratonajava/javacore/YGenerics/test/WildCardTest02.java
@@ -6,16 +6,22 @@ import java.util.List;
 
 public class WildCardTest02 {
     public static void main(String[] args) {
-        List<Cachorro> cachorros = new ArrayList<>();
-        List<Gato> gatos = new ArrayList<>();
+        List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
+        List<Gato> gatos = List.of(new Gato(), new Gato());
+        printConsulta(cachorros);
+        printConsulta(gatos);
+        List<Animal> animals = new ArrayList<>();
+        printConsultaAnimal(animals);
 
 
     }
-    private static void printConsulta(List<Animal> animals){
+    private static void printConsulta(List<? extends Animal> animals){
         for (Animal animal : animals) {
             animal.consulta();
         }
+    }
+    private static void printConsultaAnimal(List<? super Animal> animals){
         animals.add(new Cachorro());
-
+        animals.add(new Gato());
     }
 }
