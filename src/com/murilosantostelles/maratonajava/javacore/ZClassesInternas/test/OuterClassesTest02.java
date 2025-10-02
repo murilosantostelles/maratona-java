179d335d803b9d5a3f4d054136663fa1220f6fd4
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-10-02 13:56:32 -0300

    feat: Classes internas pt 02 - Classes Locais

---
+++ src/com/murilosantostelles/maratonajava/javacore/ZClassesInternas/test/OuterClassesTest02.java
@@ -0,0 +1,16 @@
+package com.murilosantostelles.maratonajava.javacore.ZClassesInternas.test;
+
+public class OuterClassesTest02 {
+    // Quando tem uma classe dentro de um metodo (pouco utilizado)
+    private String nome = "Modiriya";
+
+    void print(){
+        class LocalCLass {
+            
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
