9cec261d8f21cbe3a9cb631b4521b4719bdcd65b
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-10-05 21:17:42 -0300

    feat: Lambdas pt 03 - Function

---
+++ src/com/murilosantostelles/maratonajava/javacore/ZZBlambdas/test/LambdaTest02.java
@@ -0,0 +1,25 @@
+package com.murilosantostelles.maratonajava.javacore.ZZBlambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class LambdaTest02 {
+    public static void main(String[] args) {
+        List<String> strings = List.of("Natsu", "Allucard");
+
+        List<Integer> integers = map(strings, (String s) -> s.length());
+        List<String> upper = map(strings, s -> s.toUpperCase());
+
+        System.out.println(integers);
+        System.out.println(upper);
+    }
+    private static <T, R> List<R> map(List<T> list, Function<T, R> function){
+        List<R> result = new ArrayList<>();
+        for (T t : list) {
+            R r = function.apply(t);
+            result.add(r);
+        }
+        return result;
+    }
+}
