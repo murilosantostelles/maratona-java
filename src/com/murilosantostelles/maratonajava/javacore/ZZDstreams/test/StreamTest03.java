b148018e49b6d26ee0f6519bf28ef9ace021643d
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-10-06 14:30:19 -0300

    feat: Streams pt 03 - Introduction pt 03

---
+++ src/com/murilosantostelles/maratonajava/javacore/ZZDstreams/test/StreamTest03.java
@@ -0,0 +1,28 @@
+package com.murilosantostelles.maratonajava.javacore.ZZDstreams.test;
+
+import com.murilosantostelles.maratonajava.javacore.ZZDstreams.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest03 {
+    private static List<LightNovel> lightNovels = new ArrayList<>( List.of(new LightNovel("Tensei Shittara" , 8.99)
+            , new LightNovel("Overlord" , 3.99),
+            new LightNovel("Violet Evergarden" , 5.99) ,
+            new LightNovel("No game no life" , 2.99),
+            new LightNovel("Fullmetal Alchemist" , 5.99),
+            new LightNovel("Kumo Desuga" , 1.99),
+            new LightNovel("Monogatari" , 4.00)
+    ));
+
+    public static void main(String[] args) {
+        Stream<LightNovel> stream = lightNovels.stream();
+        long count = stream.filter(ln -> ln.getPrice() <= 4)
+                .count();
+
+        System.out.println(count);
+
+
+    }
+}
