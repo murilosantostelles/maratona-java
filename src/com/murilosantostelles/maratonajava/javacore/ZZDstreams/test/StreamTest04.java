7ceabf4445fd62acc2d7af0dca346950cc8f4069
Murilo Santos Telles <murilosantos.work@gmail.com>
2025-10-07 08:59:34 -0300

    feat: Streams pt 04 - FlatMap pt 01

---
+++ src/com/murilosantostelles/maratonajava/javacore/ZZDstreams/test/StreamTest04.java
@@ -0,0 +1,29 @@
+package com.murilosantostelles.maratonajava.javacore.ZZDstreams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTest04 {
+    public static void main(String[] args) {
+        List<List<String>> devdojo = new ArrayList<>();
+        List<String> graphicDesigners = List.of("Wildnei Suane" , "Catarina Santos", "Sandy Caarolina");
+        List<String> developers = List.of("William" , "David" , "Harrison");
+        List<String> estudantes = List.of("Édipo", "Gustavo lima" , "Gustavo Mendes", "Guilherme");
+        devdojo.add(graphicDesigners);
+        devdojo.add(developers);
+        devdojo.add(estudantes);
+        for (List<String> people : devdojo) {
+            for (String person : people) {
+                System.out.println(person);
+            }
+        }
+        System.out.println("----------------");
+        // agora fazendo com stream
+
+        devdojo.stream()
+                .flatMap(Collection::stream).forEach(System.out::println);
+        
+    }
+}
