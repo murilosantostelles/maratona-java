4b0531600995ddc4058cfbed3945f7031a292537
Murilo Santos Telles <murilosantos.work@gmail.com>
2026-01-12 19:36:44 -0300

    feat: JDBC pt 25 - CRUD pt 02 - delete

--- src/com/murilosantostelles/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
+++ src/com/murilosantostelles/maratonajava/javacore/ZZJcrud/repository/ProducerRepository.java
@@ -1,26 +1,22 @@
 package com.murilosantostelles.maratonajava.javacore.ZZJcrud.repository;
 
 
-
 import com.murilosantostelles.maratonajava.javacore.ZZJcrud.conn.ConnectionFactory;
 import com.murilosantostelles.maratonajava.javacore.ZZJcrud.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
     public static List<Producer> findByName(String name) {
-        System.out.println("Finding Producers by name: "+name);
+        System.out.println("Finding Producers by name: " + name);
         List<Producer> producers = new ArrayList<>();
         try (Connection conn = ConnectionFactory.getConnection();
              PreparedStatement ps = createPrepareStatementFindByName(conn, name);
-             ResultSet rs = ps.executeQuery();){
+             ResultSet rs = ps.executeQuery();) {
             while (rs.next()) {
                 var id = rs.getInt("id");
                 var nameProducer = rs.getString("name");
@@ -38,6 +34,23 @@ public class ProducerRepository {
         String sql = "SELECT * FROM anime_store.producer where name like ?;";
         PreparedStatement ps = conn.prepareStatement(sql);
         ps.setString(1, String.format("%%%s%%", name));
+        return ps;
+    }
+
+    public static void delete(int id) {
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement ps = createPrepareStatementDelete(conn, id)) {
+            ps.execute();
+            log.info("Deleted producer '{}' from the database", id);
+        } catch (SQLException e) {
+            log.error("Error while trying to delete producer '{}'", id, e);
+        }
+    }
+
+    private static PreparedStatement createPrepareStatementDelete(Connection conn, Integer id) throws SQLException {
+        String sql = "SELECT * FROM anime_store.producer where name like ?;";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, id);
         return ps;
     }
 }
